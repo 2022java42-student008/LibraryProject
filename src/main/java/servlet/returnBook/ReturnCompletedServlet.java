5a493bb728d3fff44d1084116a385dfaf2726b81
WIN-LP1D9G4P2QK\student <2022java42-student019@la-study.com>
2022-05-25 11:10:22 +0900

    更新

--- src/main/java/servlet/returnBook/ReturnCompletedServlet.java
+++ src/main/java/servlet/returnBook/ReturnCompletedServlet.java
@@ -8,13 +8,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.RentalBean;
+import dao.DAOException;
+import dao.RentalDAO;
 
 @WebServlet("/ReturnCompletedServlet")
 public class ReturnCompletedServlet extends HttpServlet {
  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-	    String sql = "UPDATE rental SET return_date='CURRENT_DAT' WHERE rental_id=?   ";
-	
+	   try {
+	    HttpSession session = request.getSession(false);
+        Integer num1 = (Integer)session.getAttribute("rentalInfo");
+	    RentalBean bean = (RentalBean)session.getAttribute("iRentalID");
+	    RentalDAO dao = new RentalDAO();
+	    
+	   }catch (DAOException e) {
+		   
+	   }
+	   
         RequestDispatcher rd = request.getRequestDispatcher("/./ReturnDiscriminant.jsp");
         rd.forward(request, response);
  
